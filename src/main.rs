81998195f90b86397dfead02c45995795b40d9f4
Raoni <raoni.silvestre@hotmail.com>
2024-04-23 10:22:58 -0300

    adding actix web to project

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() {
-    println!("Hello, world!!");
-    println!("This is my really cool project!")
+use actix_web::{get, post, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World!");
+    format!("Hello {}!", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
