bc8a54c9aeb0e808ea52077c773abd306ded1088
Raoni <raoni.silvestre@hotmail.com>
2024-04-24 09:29:38 -0300

    health_check endpoint and tests ok

---
+++ tests/health_check.rs
@@ -0,0 +1,24 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(&format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute the health_check request!");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Erro ao bindar o endereço");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
