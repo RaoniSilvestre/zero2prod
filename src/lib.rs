06e6818e1bdda412ca1713e3bf10378480d2ea0b
Raoni <raoni.silvestre@hotmail.com>
2024-04-24 11:43:45 -0300

    /subscriptions tests and initial implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,38 @@
+// ------------------------ IMPORTS ------------------------------------
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
 
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+// ------------------------ TYPES --------------------------------------
 
-async fn health_check() -> impl Responder {
+#[derive(serde::Deserialize)]
+struct FormData {
+    _email: String,
+    _name: String,
+}
+
+// ------------------------ AUX FUNC  ----------------------------------
+
+// ------------------------ ENDPOINTS ----------------------------------
+
+async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+// ----------------------- RUN SERVER ----------------------------------
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
