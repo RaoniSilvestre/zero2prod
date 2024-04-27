5dff317deddd90966c9cf5b3e813365fc0a672dc
Raoni <raoni.silvestre@hotmail.com>
2024-04-27 11:45:57 -0300

    add tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -12,16 +12,18 @@ pub struct FormData {
 pub async fn subscribe(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
 
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding '{}' '{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
+
+    tracing::info!(
         "request_id {} - Saving new subscriber details in the database",
         request_id
     );
+
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -36,14 +38,14 @@ pub async fn subscribe(form: web::Form<FormData>, pg_pool: web::Data<PgPool>) ->
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e
