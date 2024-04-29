681e7f7e3b2d3026187639286507c0e8cac56d6f
Raoni <raoni.silvestre@hotmail.com>
2024-04-29 12:21:50 -0300

    Creating telemetry mod

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,12 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    let subscriber = get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration");
 
